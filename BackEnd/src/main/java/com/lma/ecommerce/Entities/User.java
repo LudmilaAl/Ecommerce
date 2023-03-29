cc2f6080d627044c85f5dbd28d4bd41609beb192
LudmilaAl <109355494+LudmilaAl@users.noreply.github.com>
2023-03-29 11:31:03 -0300

    User Entity

---
+++ BackEnd/src/main/java/com/lma/ecommerce/Entities/User.java
@@ -0,0 +1,148 @@
+package com.lma.ecommerce.Entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    //Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String lastname;
+
+    @NotBlank
+    @Size(min = 8, max = 20)
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    private String country;
+
+    private String city;
+
+    private String address;
+
+    @NotBlank
+    @Size(min = 8, max = 30)
+    private String password;
+
+    //Constructors
+    public User() {
+    }
+
+    public User(Long id, String name, String lastname, String username, String email, String country, String city, String address, String password) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.username = username;
+        this.email = email;
+        this.country = country;
+        this.city = city;
+        this.address = address;
+        this.password = password;
+    }
+
+    //Getters and setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //ToString
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", address='" + address + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
